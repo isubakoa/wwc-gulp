7b725a56e151c68487d16af198a1378ea541cd30
Ghis <fata.suir9@gmail.com>
2015-09-05 15:50:47 +0800

    update gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+
+//Convert Jade to Html
+gulp.task('jade',function(){
+});
+
+//Launch server
+gulp.task('connect', function(){
+});
+
+//Convert Sass to Css and Minify it
+gulp.task('sass', function(){
+});
+
+//Convert Coffeescript to Js, Lint it and Uglify it
+gulp.task('coffee', function(){
+});
+
+//Compress images
+gulp.task('image', function(){
+});
+
+//Supervise
+gulp.task('watch', function(){
+});
+
+//Deployment
+gulp.task('deploy');
+
+//Default
+gulp.task('default');
